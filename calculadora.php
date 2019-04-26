2d8882e81476a68ca4a50dec0b84c5681622a2dc
jbusca87 <49550772+jbusca87@users.noreply.github.com>
2019-04-26 10:24:22 +0200

    s

--- calculadora.php
+++ calculadora.php
@@ -1,3 +1,23 @@
+<?php 
+	if ($_POST ["valor1"] !="" and $_POST ["valor2"]!=""){
+		if ($_POST["operador"] == "suma") {
+			print ($resultado = $_POST ["valor1"] + $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "resta") {
+			print ($resultado = $_POST ["valor1"] - $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "multiplicacion") {
+			print ($resultado = $_POST ["valor1"] * $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "division") {
+			print ($resultado = $_POST ["valor1"] / $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		}
+	} else {
+		print("Ingresa alg&uacute;n valor");
+		print ('<br /><a href="calculadora.php">Volver</a>');
+	}
+?>
 <html>
         <head>
             <title>Mi primera Calculadora</title>
@@ -7,9 +27,18 @@
     <body>
             <h1>Calculadora</h1>
             
-            <form action="" method="post">
-
-            <option value=suma 
-
+                <select name="operador">
+				    <option value="suma">Suma</option>
+				    <option value="resta">Resta</option>
+				    <option value="multiplicacion">Multiplicación</option>
+				    <option value="division">Division</option>
+			    </select><br />
+			            Ingresa tu primer número:<br />
+			            <input type="text" name="valor1"><br />
+			            Ingresa tu segundo valor:<br />
+			        <input type="text" name="valor2"><br>
+			        <input type="reset" value="Borrar">
+			        <input type="submit" value="Enviar">
+		</form>
     </body>
 </html>    
